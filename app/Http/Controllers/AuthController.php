1a0b9bfd968ac4dba8de6ecf0986f1009e565e53
Hasan <akhmadkhasan68@gmail.com>
2021-04-27 06:04:10 +0700

    auth with username or email

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,23 +5,41 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $email = $request->email;
+        $login = $request->login;
         $password = $request->password;
         
-        if(empty($email) || empty($password))
+        $attrs = [
+            'login' => 'E-Mail or Username',
+            'password' => 'Password'
+        ];
+        $validator = Validator::make($request->all(), [
+            'login' => 'required',
+            'password' => 'required|min:8'
+        ], [], $attrs);
+
+        if($validator->fails())
         {
+            $errors = $validator->errors();
             return response()->json([
                 'error' => true,
+                'form_error' => $errors->all(),
                 'message' => 'You must fill all the fields'
             ], 422);
         }
-        $credentials = request(['email', 'password']);
+
+        $login_type = filter_var($request->input('login'), FILTER_VALIDATE_EMAIL ) ? 'email' : 'username';
+
+        $request->merge([
+            $login_type => $request->input('login')
+        ]);
+
+        $credentials = request([$login_type, 'password']);
 
         if (!$token = Auth::attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
