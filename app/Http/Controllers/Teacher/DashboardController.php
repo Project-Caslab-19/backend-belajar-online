eb1e0a62b469130a60224c65d39abba160691c0c
hasan <akhmadkhasan68@gmail.com>
2021-05-21 01:30:15 +0700

    update response dashboard teacher endpoint

--- app/Http/Controllers/Teacher/DashboardController.php
+++ app/Http/Controllers/Teacher/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Teacher;
 use App\Http\Controllers\Controller;
 use App\Helpers\ResponseHelper;
 use App\Models\ClassMember;
+use App\Models\Classroom;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,16 @@ class DashboardController extends Controller
     public function get_dashboard_content(){
         $user_id = Auth::user()->id;
         try{
-            $data = ClassMember::with('classroom')->where('user_id', $user_id)->get();
+            $classroom = Classroom::where('user_id', $user_id)->get();
+            $class_members = ClassMember::with('classroom')->whereHas('classroom', function($q) use($user_id){
+                $q->where('user_id', $user_id);
+            })->get();
+
+            $data = [
+                'total_classroom' => $classroom->count(),
+                'total_members' => $class_members->count(),
+            ];
+
             return ResponseHelper::responseSuccessWithData($data);
         }catch(\Exception $ex){
             return ResponseHelper::responseError($ex, 500);
